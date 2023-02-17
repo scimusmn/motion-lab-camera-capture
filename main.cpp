adb429df465d55f92b8a24e37ab309abacafc9c7
sanine-a <sanine.not@pm.me>
2023-02-17 12:32:37 -0600

    clean up & comment main.cpp

--- main.cpp
+++ main.cpp
@@ -14,76 +14,43 @@ using namespace VWSDK;
 
 #define IMAGE_WIDTH  640
 #define IMAGE_HEIGHT 480
-#define FRAMES 800
+#define FRAME_COUNT 800
 #define WRITE_THREADS 8
 
 
+// thread pool for writing files
 ctpl::thread_pool write_pool(WRITE_THREADS);
+// callback for processing frames
+void frame_callback(OBJECT_INFO *object, IMAGE_INFO *image);
+// count of the number of processed frames
+atomic_long frame_count = 0;
+// file prefix (used for path information)
+const char *prefix;
 
 
+// macro that defines how to release GigE resources
+// it is modified over the course of the program as resources are loaded
 #define RELEASE_RESOURCES
+
+// macro to check for errors
+// it uses RELEASE_RESOURCES to free GigE resources in case of errors
 #define CHECK_ERROR(result, activity) \
 do { \
 	if (result != RESULT_SUCCESS) { \
 		fprintf(stderr, "[FATAL] Failed to %s: %s (%d)\n", activity, result_to_string(result), result); \
 		RELEASE_RESOURCES; \
-		exit(1); \
+		return 1; \
 	} \
 } while (0)
 
 
-static void write(unsigned char byte, void *userdata) { fputc(byte, (FILE*) userdata); }
-
-
-struct Image {
-	unsigned int index;
-	unsigned int w, h;
-	unsigned char *data;
-
-	void ConvertInfo(IMAGE_INFO *info) {
-		w = info->width;
-		h = info->height;
-		data = new unsigned char[w * h * 3];
-		memcpy(data, info->pImage, w * h * 3);
-	}
-};
-
-
-void write_image(int thread, const char *prefix, Image img) {
-	char filename[32];
-	snprintf(filename, 32, "%s%03d.jpg", prefix, img.index);
-	FILE *writer = fopen(filename, "wb");
-	if (writer != NULL) {
-		TooJpeg::writeJpeg(write, writer, img.data, img.w, img.h, true, 90, false, "hello");
-		fclose(writer);
-	}
-	else {
-		fprintf(stderr, "[ERROR] Could not open \"%s\"\n", filename);
-	}
-
-	free(img.data);
-}
-
-
-atomic_long snaps = 0;
-std::queue<Image> images;
-const char *prefix;
-
-void image_callback(OBJECT_INFO *object, IMAGE_INFO *image) {
-	if (snaps >= FRAMES) { return; }
-	Image img;
-	img.index = snaps;
-	img.ConvertInfo(image);
-	write_pool.push(write_image, prefix, img);
-	printf("pushed %d\n", (unsigned int ) snaps);
-	snaps += 1;
-}
-
+// --===== main =====--
 
 int main(int argc, char **argv) {
+	// get the file prefix (used to prepend "tempXXX\" to the filename)
 	if (argc < 2) {
 		fprintf(stderr, "[FATAL] No file prefix specified!\n");
-		exit(1);
+		return 1;
 	}
 	prefix = argv[1];
 
@@ -101,7 +68,7 @@ int main(int argc, char **argv) {
 	if (numCameras == 0) {
 		fprintf(stderr, "[FATAL] No cameras were found!\n");
 		RELEASE_RESOURCES;
-		exit(1);
+		return 1;
 	}
 
 	// open camera
@@ -114,7 +81,7 @@ int main(int argc, char **argv) {
 		20, // number of buffers
 		0, 0, 0, // width/height/packet size (zero for auto-detect)
 		&user, // user pointer (CANNOT BE NULL OR THIS FUNCTION WILL ERROR FOR SOME REASON)
-		image_callback
+		frame_callback
 	);
 	CHECK_ERROR(result, "open camera");
 
@@ -136,8 +103,8 @@ int main(int argc, char **argv) {
 	// grab images
 	result = CameraGrab(camera);
 	CHECK_ERROR(result, "begin acquiring images");
-	while(snaps < FRAMES) { /* wait for frames */ }
-	RELEASE_RESOURCES;
+	while(frame_count < FRAME_COUNT) { /* wait for frames */ }
+	RELEASE_RESOURCES; // release camera & gigE driver
 
 	// wait for writes to complete
 	printf("Acquiring finished, waiting for filesystem...\n");
@@ -145,4 +112,63 @@ int main(int argc, char **argv) {
 
 	printf("Done!\n");
 	return 0;
+}
+
+
+// --===== etc =====--
+
+// image data struct
+struct Image {
+	unsigned int index;
+	unsigned int w, h;
+	unsigned char *data;
+
+	void ConvertInfo(IMAGE_INFO *info) {
+		w = info->width;
+		h = info->height;
+		data = new unsigned char[w * h * 3];
+		memcpy(data, info->pImage, w * h * 3);
+	}
+};
+
+
+// helper function for writing toojpeg output to disk
+static void write(unsigned char byte, void *userdata) { fputc(byte, (FILE*) userdata); }
+
+// write Image to disk, called from thread pool
+void write_image(int unused, const char *prefix, Image img) {
+	// compute filename as [prefix] + [number].jpg
+	char filename[32];
+	snprintf(filename, 32, "%s%03d.jpg", prefix, img.index);
+
+	// open file
+	FILE *writer = fopen(filename, "wb");
+	if (writer != NULL) {
+		// write jpeg data to disk
+		TooJpeg::writeJpeg(write, writer, img.data, img.w, img.h, true, 90, false, "hello");
+		fclose(writer);
+	}
+	else {
+		fprintf(stderr, "[ERROR] Could not open \"%s\"\n", filename);
+	}
+
+	free(img.data);
+}
+
+
+// per-frame callback
+void frame_callback(OBJECT_INFO *unused, IMAGE_INFO *image) {
+	// check if we have alread collected the frames we need
+	if (frame_count >= FRAME_COUNT) { return; }
+	
+	// copy the image data into an Image struct
+	Image img;
+	img.index = frame_count;
+	img.ConvertInfo(image);
+
+	// push to the thread pool
+	write_pool.push(write_image, prefix, img);
+
+	// increment frame count
+	frame_count += 1;
 }
